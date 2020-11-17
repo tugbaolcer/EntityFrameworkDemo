eac89335125b622ed70c0106a13f853452a7fb4d
tugbaolcer <35061098+tugbaolcer@users.noreply.github.com>
2020-11-17 10:36:27 +0300

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EntityFrameworkDemo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            LoadProducts();
+        }
+
+        ProductDal _productDal = new ProductDal();
+
+   
+        private void Form1_Load(object sender, EventArgs e)
+        { 
+        }
+
+
+        private void LoadProducts()
+        {
+            dgwProducts.DataSource = _productDal.GetAll();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            _productDal.Add(new Product
+            {
+               
+                Name = tbxName.Text,
+                UnitPrice = Convert.ToDecimal(tbxUnitPrice.Text),
+                StockAmount = Convert.ToInt32(tbxStockAmount.Text)
+            });
+            MessageBox.Show("Added !");
+            LoadProducts();
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            _productDal.Update(new Product
+            {
+                Id=Convert.ToInt32(dgwProducts.CurrentRow.Cells[0].Value),
+                Name=tbxNameUpdate.Text,
+                UnitPrice=Convert.ToDecimal(tbxUnitPriceUpdate.Text),
+                StockAmount=Convert.ToInt32(tbxStockAmountUpdate.Text)
+            });
+            MessageBox.Show("Updated !");
+            LoadProducts();
+        }
+
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+            _productDal.Delete(new Product
+            {
+                Id=Convert.ToInt32(dgwProducts.CurrentRow.Cells[0].Value)
+            });
+            LoadProducts();
+            MessageBox.Show("Removed!");
+        }
+
+        private void tbxSearch_TextChanged(object sender, EventArgs e)
+        {
+            SearchProducts(tbxSearch.Text);
+        }
+
+
+        private void SearchProducts(string key)
+        {
+            //dgwProducts.DataSource = _productDal.GetAll().Where(p=>p.Name.ToLower().Contains(key.ToLower())).ToList();
+            /*
+             Yukarıdaki gibi koleksiyondan veri alınacağı zaman büyük harf küçük harf duyarlılığı oluşur.
+             Bunu gidermek adına koleksiyondaki ismi tolower ile kiçik harfe dönüştürürüz. Sonrasında dışarıdan gelen keyi de küçük harfe dönüştürerek
+             bu hasassiyetten kurtulmuş oluruz.
+             
+             */
+
+
+            //Direk veritabanından böyle çekilir.
+            var result = _productDal.GetByName(key);
+            dgwProducts.DataSource = result;
+
+            /*Koleskisyonlarda oluşan büyük harf küçük harf duyarlılığı veritabanından çektiğimizde oluşmamaktadır.*/
+        }
+
+        private void dgwProducts_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            tbxNameUpdate.Text = dgwProducts.CurrentRow.Cells[1].Value.ToString();
+            tbxUnitPriceUpdate.Text = dgwProducts.CurrentRow.Cells[2].Value.ToString();
+            tbxStockAmountUpdate.Text = dgwProducts.CurrentRow.Cells[3].Value.ToString();
+        }
+
+        private void btnGetById_Click(object sender, EventArgs e)
+        {
+            _productDal.GetById(1);
+        }
+    }
+}
---
+++ ProductDal.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkDemo
+{
+    public class ProductDal
+    {
+        public List<Product> GetAll()
+        {
+            using (EtradeContext context = new EtradeContext()) //Using, Etrade nesnesi bellekte fazla yer kapladığı için iş bitiminde garbage collectoru beklemeden nesneyi zorla bellekten atar.
+            {
+                return context.Products.ToList(); //Entitiy frameworkta veritabanındaki verileri listeler.
+            }
+
+        }
+
+        public List<Product> GetByName(string key)//Koleksiyona göre daha performanslıdır.
+        {
+            using (EtradeContext context = new EtradeContext()) 
+            {
+                return context.Products.Where(p => p.Name.Contains(key)).ToList();
+            }
+
+        }
+
+        //Ürün fiyatına göre listeler.
+        public List<Product> GetByUnitPrice(decimal min, decimal max)
+        {
+            using (EtradeContext context = new EtradeContext())
+            {
+                return context.Products.Where(p => p.UnitPrice >= min &&p.UnitPrice<=max).ToList();
+                
+            }
+
+        }
+
+        //Tek bir ürün getirme
+        public Product GetById(int id)
+        {
+            using (EtradeContext context = new EtradeContext())
+            {
+                return context.Products.FirstOrDefault(p => p.Id == id);//Bu id ye uygun ilk veriyi getir. Veri yoksa default dön.
+            }
+
+        }
+        public void Add(Product product)
+        {
+            using (EtradeContext context = new EtradeContext()) 
+            {
+                context.Products.Add(product);
+                context.SaveChanges();
+            }
+        }
+
+        public void Update(Product product)
+        {
+            using (EtradeContext context = new EtradeContext())
+            {
+                var entity = context.Entry(product); // gönderilen product'i veritabanındaki product ile eşitler.
+                entity.State = EntityState.Modified; // durumunu güncellendi olarak işaretler.
+
+
+                context.SaveChanges();
+            }
+        }
+
+        public void Delete(Product product)
+        {
+            using (EtradeContext context = new EtradeContext())
+            {
+                var entity = context.Entry(product); // gönderilen product'i veritabanındaki product ile eşitler.
+                entity.State = EntityState.Deleted; // durumunu güncellendi olarak işaretler.
+
+
+                context.SaveChanges();
+            }
+        }
+    }
+}
